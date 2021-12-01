d253875b312d375db64a549d4096bec1265fafdd
Tianya Chen <tianyac@andrew.cmu.edu>
2021-11-30 21:59:03 -0500

    change folder layout

---
+++ pairwise2/main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import distutils.util
+
+import pairwise2
+import pairwise2simd
+
+sequenceA = "ACCGTACACGTAGC"
+sequenceB = "ACGTCGA"
+
+# alignments = pairwise2.align.globalxx(sequenceA=sequenceA, sequenceB=sequenceB)
+# alignments_simd = pairwise2simd.align.globalxx(sequenceA=sequenceA, sequenceB=sequenceB)
+alignments = pairwise2.align.globalms(sequenceA=sequenceA, sequenceB=sequenceB, match=1, mismatch=-1, score_only=True, open=-1, extend=0)
+alignments_simd = pairwise2simd.align.globalms(sequenceA=sequenceA, sequenceB=sequenceB, match=1, mismatch=-1, score_only=True, open=-1, extend=0)
+print(alignments == alignments_simd)
+print(alignments)
+print(alignments_simd)
