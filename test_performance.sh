1e963f167e575b3d6458b3a8ca95fab719115eb7
lichengkai <lck1024@outlook.com>
2021-12-06 03:06:59 -0500

    delete useless code and create test shell script
    delete useless code and create test shell script

--- test_performance.sh
+++ test_performance.sh
@@ -1,14 +1,9 @@
 gcc -c generate.c -o generate.o
 gcc generate.o -o generate
-./generate 32 32
+./generate 16384 32
 rm generate
 rm generate.o
 cd test_kernels
 make
 make clean
-cd ../pairwise2
-make
-python3 main.py
-make clean
 cd ..
-diff kernel_res.txt base_res.txt
